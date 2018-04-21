37d408c3956123d9e7f629d6255e80f887806296
Andrew Jalbert <andrewjalbert@ymail.com>
2018-04-20 22:55:40 -0400

    created function definition

--- src/binary_search.cpp
+++ src/binary_search.cpp
@@ -1,1 +1,11 @@
+// binary search algorithm
+
+
+// l is the left index (0) and r is the right index
+// val is the value to search for
+// returns -1 if no value found
+template<typename T>
+T binary_search(long l, long r, T val) {
+
+}
 

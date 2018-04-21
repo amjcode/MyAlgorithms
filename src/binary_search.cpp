24c83e3c6956b29fa05ccbfc02ef3a5dc1c0073d
Andrew Jalbert <andrewjalbert@ymail.com>
2018-04-21 13:38:00 -0400

    reorganized file

--- src/binary_search.cpp
+++ src/binary_search.cpp
@@ -4,15 +4,6 @@
 
 #include<iostream>
 
-long binary_search(long ary[], long, long);
-
-int main() {
-  long size = 16;
-  long array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-  std::cout << binary_search(array, size, 0) << std::endl;
-  std::cout << binary_search(array, size, 1) << std::endl;
-}
-
 // returns the index where val was found or -1 if it wasn't found
 long binary_search(long ary[], long size, long val) {
   long l = 0; // left index
@@ -28,4 +19,11 @@ long binary_search(long ary[], long size, long val) {
       return mid;
   }
   return -1;
+}
+
+int main() {
+  long size = 16;
+  long array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+  std::cout << binary_search(array, size, 0) << std::endl;
+  std::cout << binary_search(array, size, 1) << std::endl;
 }

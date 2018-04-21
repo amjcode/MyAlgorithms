d50f702648e45c7cd47a87bdbf0204694ccc801d
Andrew Jalbert <andrewjalbert@ymail.com>
2018-04-20 23:55:58 -0400

    it works

--- src/binary_search.cpp
+++ src/binary_search.cpp
@@ -5,12 +5,21 @@
 #include<iostream>
 using namespace std;
 
+long binary_search(long ary[], long, long, long);
+
+int main() {
+  long l = 0;
+  long r = 15;
+  long val = 7;
+  long array[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+  cout << binary_search(array, l, r, val) << endl;
+}
+
 // l is the left index (0) and r is the right index
 // val is the value to search for
 // returns -1 if no value found
-template<typename T>
-T binary_search(T[] ary, long l, long r, T val) {
-  long mid = (l + r) / 2;
+long binary_search(long ary[], long l, long r, long val) {
+  long mid = l + (r - l) / 2;
 
   while(ary[mid] != val){
     if(ary[mid] < val) {
@@ -19,9 +28,7 @@ T binary_search(T[] ary, long l, long r, T val) {
     } else if(ary[mid] > val) {
       r = mid;
       mid -= (r - l) / 2;
-    } else {
-      return mid;
     }
   }
-
+  return mid;
 }

987568ac972e76837a3ad9ea04000853cb86b099
Andrew Jalbert <andrewjalbert@ymail.com>
2018-04-22 01:02:18 -0400

    euclids algorithm code added

--- src/math/euclids_algorithm.cpp
+++ src/math/euclids_algorithm.cpp
@@ -1,1 +1,14 @@
 // euclid's algorithm
+#include<iostream>
+
+long gcd(long p, long q) {
+  if (q == 0) return p;
+  int r = p % q;
+  return gcd(q, r);
+}
+
+int main() {
+  long p = 23;
+  long q = 444;
+  std::cout << gcd(p, q) << std::endl;
+}
